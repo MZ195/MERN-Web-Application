b0cf4eddce501491e4ec2d085ad0bdae48616d88
Mazen.A <40984264+MZ195@users.noreply.github.com>
2020-07-31 14:26:23 +0300

    Profile schema is ready

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require("passport");
 
 // load input validation
 const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
 
 // load user model
 const User = require("../../models/User");
@@ -74,6 +75,15 @@ router.post("/register", (req, res) => {
 // @desc    Login user, Returning the JWT token
 // @access  Public
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json({
+      errors,
+    });
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -81,7 +91,8 @@ router.post("/login", (req, res) => {
   User.findOne({ email }).then((user) => {
     // Check for user
     if (!user) {
-      return res.status(404).json({ email: "user not found" });
+      errors.email = "user not found";
+      return res.status(404).json({ errors });
     }
 
     // Check password
@@ -109,7 +120,8 @@ router.post("/login", (req, res) => {
           }
         );
       } else {
-        return res.status(400).json({ password: "Password incorrect!" });
+        errors.password = "Password incorrect!";
+        return res.status(400).json({ errors });
       }
     });
   });
