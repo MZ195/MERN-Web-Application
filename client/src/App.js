357205914169fea75d3e8f54df881246763a7aa1
Mazen.A <40984264+MZ195@users.noreply.github.com>
2020-08-09 16:41:40 +0300

    add/remove comments is Done.

--- client/src/App.js
+++ client/src/App.js
@@ -24,6 +24,7 @@ import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
 import NotFound from "./components/not-found/NotFound";
 import Posts from "./components/posts/Posts";
+import Post from "./components/post/Post";
 
 import PrivateRoute from "./components/common/PrivateRoute";
 
@@ -93,6 +94,9 @@ function App() {
             </Switch>
             <Switch>
               <PrivateRoute exact path="/feed" component={Posts} />
+            </Switch>
+            <Switch>
+              <PrivateRoute exact path="/post/:id" component={Post} />
             </Switch>
             <Route exact path="/not-found" component={NotFound} />
           </div>
