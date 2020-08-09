8d4b0a3c8c2ef7f8af5802e4a97ab48d9525fc96
Mazen.A <40984264+MZ195@users.noreply.github.com>
2020-08-09 12:59:10 +0300

    Create new post is Done

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,7 +1,16 @@
 export const GET_ERRORS = "GET_ERRORS";
+
 export const GET_PROFILE = "GET_PROFILE";
 export const GET_PROFILES = "GET_PROFILES";
 export const PROFILE_LOADING = "PROFILE_LOADING";
-export const SET_CURRENT_USER = "SET_CURRENT_USER";
 export const PROFILE_NOT_FOUND = "PROFILE_NOT_FOUND";
+
+export const ADD_POST = "ADD_POST";
+export const GET_POST = "GET_POST";
+export const GET_POSTS = "GET_POSTS";
+export const POST_LOADING = "POST_LOADING";
+
+export const DELETE_POST = "DELETE_POST";
+
+export const SET_CURRENT_USER = "SET_CURRENT_USER";
 export const CLEAR_CURRENT_PROFILE = "CLEAR_CURRENT_PROFILE";
